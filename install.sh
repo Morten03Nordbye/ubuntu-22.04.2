4601131dcc4789ec8b0773e1749ec13dc36919d9
Morten Victor Nordbye <74780083+Morten03Nordbye@users.noreply.github.com>
2023-03-27 13:43:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ flatpak install flathub org.remmina.Remmina -y
 flatpak install flathub com.visualstudio.code -y
 
 # Install other packages using apt
-sudo apt install flameshot libreoffice vim nitrogen rofi i3 arandr terminator -y
+sudo apt install flameshot libreoffice vim nitrogen rofi i3 arandr terminator xclip -y
 
 # Install Polybar
 sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
